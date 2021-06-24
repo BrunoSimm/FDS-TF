5394023d7beddeecd4f82b5ed31bf127eb0432ac
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-06-24 23:53:00 +0000

    Interface IRepositorioComentarios

--- projetoTF/src/main/java/com/grupofds/projetoTF/negocio/repositorios/IRepositorioComentarios.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/negocio/repositorios/IRepositorioComentarios.java
@@ -1,5 +1,14 @@
 package com.grupofds.projetoTF.negocio.repositorios;
 
-public interface IRepositorioComentarios {
+import java.util.List;
 
+public interface IRepositorioComentarios {
+    public Comentario addComentario(long usuarioId, long idReclamacao, String mensagem, String imagem, STATUS_RECLAMACOES status);
+    public Comentario editComentario(long idComentario, String mensagem , String imagem, STATUS_RECLAMACOES status);
+    public boolean deleteComentario(long idComentario);
+    public Comentario getById(long id);
+    public Comentario getById(long id);
+    public List<Comentario> getByReclamacao(long idReclamacao);
+    public List<Comentario> getByUsuario(long idUsuario);
+    public List<Comentario> getByStatus(STATUS_RECLAMACOES status);
 }
