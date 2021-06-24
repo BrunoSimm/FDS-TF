bd597e678ae25383aebcbbef6265986d469909d6
brunosimm <bruno.simm@edu.pucrs.br>
2021-06-24 18:31:17 -0300

    Estrutura inicial do projeto Spring + Dev Container

---
+++ projetoTF/src/main/java/com/grupofds/projetoTF/ProjetoTfApplication.java
@@ -0,0 +1,13 @@
+package com.grupofds.projetoTF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTfApplication.class, args);
+	}
+
+}
---
+++ projetoTF/src/test/java/com/grupofds/projetoTF/ProjetoTfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupofds.projetoTF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
