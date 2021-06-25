a542c1e85b10cc429afb27ca0f28dcac777d45f0
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-06-25 00:01:01 +0000

    Interface IRepositorioIRepositorio

--- projetoTF/src/main/java/com/grupofds/projetoTF/negocio/repositorios/IRepositorioReclamacoes.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/negocio/repositorios/IRepositorioReclamacoes.java
@@ -1,5 +1,15 @@
 package com.grupofds.projetoTF.negocio.repositorios;
 
-public interface IRepositorioReclamacoes {
+import java.time.LocalDateTime;
 
+import com.grupofds.projetoTF.negocio.entidades.Reclamacao;
+
+public interface IRepositorioReclamacoes {
+    public Reclamacao createReclamacao(Reclamacao reclamacao);
+    public Reclamacao atualizaReclamacao(Reclamacao reclamacao);
+    public Reclamacao getById(long id);
+    public List<Reclamacao> getByUsuario(long id);
+    public List<Reclamacao> getByCategoria(long idCategoria);
+    public List<Reclamacao> getByBairro(String bairro);
+    public List<Reclamacao> getByPeriodo(LocalDateTime periodo);
 }
