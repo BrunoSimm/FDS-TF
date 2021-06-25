b93522a65656a9b14eb960ef18c651f4c5b1ea12
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-06-25 03:13:23 +0000

    Classes ComentariosServicos e ServicoRelatoriosAdmin e atualizacao em IRepositorioReclamacoes

--- projetoTF/src/main/java/com/grupofds/projetoTF/aplicacao/servicos/ServicoRelatoriosAdmin.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/aplicacao/servicos/ServicoRelatoriosAdmin.java
@@ -1,5 +1,48 @@
 package com.grupofds.projetoTF.aplicacao.servicos;
 
+import java.time.LocalDateTime;
+
 public class ServicoRelatoriosAdmin {
 
+
+    public int getTotalReclamacoesByCategoria(Long usuarioId, String categoria, LocalDateTime periodo) {
+
+    }
+
+    public int getTotalReclamacoesByBairro(Long usuarioId, String bairro, LocalDateTime periodo) {
+        
+    }
+
+    public double getNumeroMedioComentariosByCategoria(Long usuarioId, String categoria) {
+
+    }
+
+    public double getNumeroMedioComentariosByBairro(Long usuarioId, String bairro) {
+        
+    }
+
+    public double getNumeroMedioComentariosByPeriodo(Long usuarioId, LocalDateTime periodo) {
+
+    }
+
+    public double getPercentualResolvidoByCategoria(Long usuarioId, String categoria) {
+
+    }
+    
+    public double getPercentualResolvidoByBairro(Long usuarioId, String bairro) {
+        
+    }
+
+    public double getPercentualEncerradoByCategoria(Long usuarioId, String categoria) {
+        
+    }
+
+    public double getPercentualEncerradoByBairro(Long usuarioId, String bairro) {
+
+    }
+
+    public double getPercentualRespondidoByUserOficial(Long idUsuarioOficial) {
+
+    }
+    
 }
