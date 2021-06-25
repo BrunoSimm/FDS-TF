c633cdf4de094bfa0ae5d336e7e29cad98e0b840
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-06-25 03:13:27 +0000

    Classes ComentariosServicos e ServicoRelatoriosAdmin e atualizacao em IRepositorioReclamacoes

--- projetoTF/src/main/java/com/grupofds/projetoTF/aplicacao/servicos/ServicoRelatoriosAdmin.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/aplicacao/servicos/ServicoRelatoriosAdmin.java
@@ -2,47 +2,62 @@ package com.grupofds.projetoTF.aplicacao.servicos;
 
 import java.time.LocalDateTime;
 
+import com.grupofds.projetoTF.negocio.repositorios.IRepositorioComentarios;
+import com.grupofds.projetoTF.negocio.repositorios.IRepositorioReclamacoes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ServicoRelatoriosAdmin {
+    private IRepositorioReclamacoes repositorioReclamacoes;
+    private IRepositorioComentarios repositorioComentarios;
 
-
-    public int getTotalReclamacoesByCategoria(Long usuarioId, String categoria, LocalDateTime periodo) {
-
+    @Autowired
+    public ServicoRelatoriosAdmin(IRepositorioReclamacoes repositorioReclamacoes,
+            IRepositorioComentarios repositorioComentarios) {
+        this.repositorioReclamacoes = repositorioReclamacoes;
+        this.repositorioComentarios = repositorioComentarios;
     }
 
-    public int getTotalReclamacoesByBairro(Long usuarioId, String bairro, LocalDateTime periodo) {
-        
+    public Long getTotalReclamacoesByCategoriaAndPeriodo(Long usuarioId, String categoria, LocalDateTime periodoInicial, LocalDateTime periodoFinal) {
+        return repositorioReclamacoes.getByCategoria(categoria).stream()
+            .filter(r -> !r.getData().isBefore(periodoInicial) && !r.getData().isAfter(periodoFinal))
+            .count();
+    }
+
+    public int getTotalReclamacoesByBairroAndPeriodo(Long usuarioId, String bairro, LocalDateTime periodoInicial, LocalDateTime periodoFinal) {
+        //TODO: logica
     }
 
     public double getNumeroMedioComentariosByCategoria(Long usuarioId, String categoria) {
-
+        //TODO: logica
     }
 
     public double getNumeroMedioComentariosByBairro(Long usuarioId, String bairro) {
-        
+        //TODO: logica
     }
 
     public double getNumeroMedioComentariosByPeriodo(Long usuarioId, LocalDateTime periodo) {
-
+        //TODO: logica
     }
 
     public double getPercentualResolvidoByCategoria(Long usuarioId, String categoria) {
-
+        //TODO: logica
     }
     
     public double getPercentualResolvidoByBairro(Long usuarioId, String bairro) {
-        
+        //TODO: logica        
     }
 
     public double getPercentualEncerradoByCategoria(Long usuarioId, String categoria) {
-        
+        //TODO: logica        
     }
 
     public double getPercentualEncerradoByBairro(Long usuarioId, String bairro) {
-
+        //TODO: logica
     }
 
     public double getPercentualRespondidoByUserOficial(Long idUsuarioOficial) {
-
+        //TODO: logica
     }
     
 }
