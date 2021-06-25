56f649cfed10f47dfef483466dc4046ac12e75d1
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-06-25 04:38:25 +0000

    Update ServicoRelatoriosAdmin

--- projetoTF/src/main/java/com/grupofds/projetoTF/aplicacao/servicos/ServicoRelatoriosAdmin.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/aplicacao/servicos/ServicoRelatoriosAdmin.java
@@ -1,7 +1,9 @@
 package com.grupofds.projetoTF.aplicacao.servicos;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.grupofds.projetoTF.negocio.entidades.Reclamacao;
 import com.grupofds.projetoTF.negocio.repositorios.IRepositorioComentarios;
 import com.grupofds.projetoTF.negocio.repositorios.IRepositorioReclamacoes;
 
@@ -19,25 +21,39 @@ public class ServicoRelatoriosAdmin {
     }
 
     public Long getTotalReclamacoesByCategoriaAndPeriodo(Long usuarioId, String categoria, LocalDateTime periodoInicial, LocalDateTime periodoFinal) {
+        // TODO: acessar repositorio de usuario
+        // TODO: cadê o getCategoriaDeUsuario???????????????
+        // if (usuario.categoria)
         return repositorioReclamacoes.getByCategoria(categoria).stream()
             .filter(r -> !r.getData().isBefore(periodoInicial) && !r.getData().isAfter(periodoFinal))
             .count();
     }
 
-    public int getTotalReclamacoesByBairroAndPeriodo(Long usuarioId, String bairro, LocalDateTime periodoInicial, LocalDateTime periodoFinal) {
-        //TODO: logica
+    public Long getTotalReclamacoesByBairroAndPeriodo(Long usuarioId, String bairro, LocalDateTime periodoInicial, LocalDateTime periodoFinal) {
+        return repositorioReclamacoes.getByBairro(bairro).stream()
+            .filter(r -> !r.getData().isBefore(periodoInicial) && !r.getData().isAfter(periodoFinal))
+            .count();
     }
 
     public double getNumeroMedioComentariosByCategoria(Long usuarioId, String categoria) {
-        //TODO: logica
+        List<Reclamacao> aux = repositorioReclamacoes.getByCategoria(categoria);
+        int totalReclamacoes = aux.size();
+        int totalComentarios = aux.stream()
+            .reduce(0, (soma, reclamacao) -> soma + repositorioComentarios.getByReclamacao(reclamacao.getId()).size(), Integer::sum);
+        return (double) (totalComentarios / totalReclamacoes);
     }
 
     public double getNumeroMedioComentariosByBairro(Long usuarioId, String bairro) {
-        //TODO: logica
+        List<Reclamacao> aux = repositorioReclamacoes.getByBairro(bairro);
+        int totalReclamacoes = aux.size();
+        int totalComentarios = aux.stream()
+            .reduce(0, (soma, reclamacao) -> soma + repositorioComentarios.getByReclamacao(reclamacao.getId()).size(), Integer::sum);
+        return (double) (totalComentarios / totalReclamacoes);
     }
 
     public double getNumeroMedioComentariosByPeriodo(Long usuarioId, LocalDateTime periodo) {
-        //TODO: logica
+        //TODO: comentario deve ter DATA/HORA?
+
     }
 
     public double getPercentualResolvidoByCategoria(Long usuarioId, String categoria) {
