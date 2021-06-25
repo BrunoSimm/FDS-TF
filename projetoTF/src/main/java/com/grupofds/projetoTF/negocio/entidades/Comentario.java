854320b218a5e360d795d9b75de8ac60db807c53
brunosimm <bruno.simm@edu.pucrs.br>
2021-06-25 00:09:41 -0300

    Implementando relacionamentos entre entidades

--- projetoTF/src/main/java/com/grupofds/projetoTF/negocio/entidades/Comentario.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/negocio/entidades/Comentario.java
@@ -1,5 +1,6 @@
 package com.grupofds.projetoTF.negocio.entidades;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,40 +8,53 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import com.grupofds.projetoTF.negocio.entidades.usuarios.Usuario;
 
 @Entity
-@Table(name = "cometarios")
+@Table(name = "comentarios")
 public class Comentario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_usuario_fk", nullable = false)
     private Usuario usuario;
 
     @Column
+    @NotBlank(message = "Descrição deve sere preenchida.")
     private String descricao;
 
     @Column(nullable = true)
     private String imagem;
 
     @ManyToOne
-    @JoinColumn(name = "id_reclamacao_fk")
+    @JoinColumn(name = "id_reclamacao_fk", nullable = false)
     private Reclamacao reclamacao;
     
-    public Comentario(Long id, Usuario usuario, String descricao, String imagem, Reclamacao reclamacao) {
-        this.id = id;
-        this.usuario = usuario;
-        this.descricao = descricao;
-        this.imagem = imagem;
-        this.reclamacao = reclamacao;
-    }
+    public Comentario(Long id, Usuario usuario, @NotBlank(message = "Descrição deve sere preenchida.") String descricao,
+			String imagem, Reclamacao reclamacao) {
+		this.id = id;
+		this.usuario = usuario;
+		this.descricao = descricao;
+		this.imagem = imagem;
+		this.reclamacao = reclamacao;
+	}
 
-    public Long getId() {
+	public Reclamacao getReclamacao() {
+		return reclamacao;
+	}
+
+	public void setReclamacao(Reclamacao reclamacao) {
+		this.reclamacao = reclamacao;
+	}
+
+	public Long getId() {
         return id;
     }
     public void setId(Long id) {
