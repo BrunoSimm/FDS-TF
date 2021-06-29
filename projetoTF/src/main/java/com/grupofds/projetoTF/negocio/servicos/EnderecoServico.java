cf792f8db3759d3415098ce9b7ff801fb51f9a00
rafaelcoll <rafael.coll@edu.pucrs.br>
2021-06-29 03:55:25 +0000

    Classe de testes

--- projetoTF/src/main/java/com/grupofds/projetoTF/negocio/servicos/EnderecoServico.java
+++ projetoTF/src/main/java/com/grupofds/projetoTF/negocio/servicos/EnderecoServico.java
@@ -10,29 +10,11 @@ import com.grupofds.projetoTF.negocio.repositorios.IRepositorioUsuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EnderecoServico {
-    private IRepositorioReclamacoes repositorioReclamacoes;
-    private IRepositorioUsuarios repositorioUsuarios;
     private IRepositorioEnderecos repositorioEnderecos;
     
     @Autowired
-    public EnderecoServico(IRepositorioReclamacoes repositorioReclamacoes,IRepositorioUsuarios repositorioUsuarios) {
-		this.repositorioReclamacoes = repositorioReclamacoes;
-		this.repositorioUsuarios = repositorioUsuarios;
-	}
-
-	public Endereco updateEndereco(Long usuarioId, Long reclamacaoId, Endereco novoEndereco) {
-    	Usuario user = repositorioUsuarios.getById(usuarioId);
-    	if (user == null) {
-            throw new IllegalArgumentException("ERRO! Usuário não encontrado. Indique um Id de Usuário válido.");
-        } 
-        Reclamacao reclamacao = repositorioReclamacoes.getById(reclamacaoId);
-        if (reclamacao == null) {
-            throw new IllegalArgumentException("ERRO! Reclamação não encontrada. Indique um Id de Reclamação válido.");
-        }
-        if (!user.equals(repositorioReclamacoes.getById(reclamacaoId).getUsuario())) {
-            throw new IllegalArgumentException("Usuário de Id " + user.getId() + " não possui permissão para atualizar esta Reclamação.");
-        } 
-        return repositorioEnderecos.updateEndereco(novoEndereco);
+    public EnderecoServico(IRepositorioEnderecos repositorioEnderecos) {
+        this.repositorioEnderecos = repositorioEnderecos;
     }
 
     public Endereco getById(Long enderecoId) {
---
+++ projetoTF/src/test/java/com/grupofds/projetoTF/ServicoRelatoriosAdminTest.java
@@ -0,0 +1,5 @@
+package com.grupofds.projetoTF;
+
+public class ServicoRelatoriosAdminTest {
+    
+}
