e5033b10622c961a3819cc41da08da55e252c722
brunosimm <bruno.simm@edu.pucrs.br>
2021-06-29 22:09:47 -0300

    Complementando dados do import.sql

--- projetoTF/src/main/resources/import.sql
+++ projetoTF/src/main/resources/import.sql
@@ -22,35 +22,35 @@ INSERT INTO enderecos (bairro, logradouro, numero, cidade, estado) VALUES ('Lamb
 
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (3,'Poste caído','Maldito poste caiu após acidente da motoca e ninguém vem consertar.','2020-3-1',1,'zh.com.br/postecaido.jpg','Iluminação Pública','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (3,'Rua alagada','Alagamento do valão não seca nunca... Vou vender minha casa!','2021-5-4',2,'zh.com.br/valao.jpg','Saneamento','ABERTA');
-INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (3,'Arvore gigante em risco de queda','','2019-12-15',3,'zh.com.br/arvorevelha.jpg','Manutenção Urbana','RESOLVIDA');
-INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (5,'Aglomeração clandestina','','2021-5-24',4,'zh.com.br/','Risco COVID-19','ABERTA');
-INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (5,'Bingo da Igreja','','2018-1-27',5,'zh.com.br/','Cuidado com Idosos','ABERTA');
+INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (3,'Arvore gigante em risco de queda','Estou com medo da arvore cair na minha casa!','2019-12-15',3,'zh.com.br/arvorevelha.jpg','Manutenção Urbana','RESOLVIDA');
+INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (5,'Aglomeração clandestina','É um absurdo uma festa deste tamanho em plena quarentena','2021-5-24',4,'zh.com.br/','Risco COVID-19','ABERTA');
+INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (5,'Bingo da Igreja','Os idosos estão brigando durante o bingo!!','2018-1-27',5,'zh.com.br/','Cuidado com Idosos','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (6,'Arraial da cidade','Joãozinho quer que promovam o arraial da cidade no mês que vem. O moloque só incomoda em casa, sem vergonha!','2021-6-24',6,'zh.com.br/','Eventos Sociais','ABERTA');
-INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (4,'Pracinha da esquina','','2021-2-2',7,'zh.com.br/','Praças Públicas','ABERTA');
+INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (4,'Pracinha da esquina','A praça está sem os cuidados básicos, a grama está alta e os banheiros estão sujos!!','2021-2-2',7,'zh.com.br/','Praças Públicas','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (4,'Grama alta','Molecada anda sumindo no esconde-esconde nesse matagal.','2020-12-24',8,'zh.com.br/','Praças Públicas','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (4,'Buraco na via','Essa cratera tá de parabéns... já comemora 5 anos e mais de 50 pneus furados!!!','2016-6-14',9,'zh.com.br/','Vias Públicas','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (6,'Calçada quebrada','Calçada tá um horror.. tão esperando alguém cair e pefir indenização?','2020-10-11',10,'zh.com.br/','Manutenção Urbana','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (6,'Terreno baldio','Relatos de plantas com cheiro característicos vindo do terreno e movimentações noturnas suspeitas.','2021-01-12',11,NULL,'Guarda municipal','ABERTA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (6,'Lixo espalhado','Panfletagem das eleições avacalhou a rua. Tá tudo espalhado e sujo... tudo ficha suja!','2020-10-15',12,'zh.com.br/','Saneamento','ABERTA');
-INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (5,'Boeiro entupido','','2019-9-2',13,'zh.com.br/','Saneamento','ENCERRADA');
+INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (5,'Boeiro entupido','Toda vez que chove acaba alagando a minha rua!!','2019-9-2',13,'zh.com.br/','Saneamento','ENCERRADA');
 INSERT INTO reclamacoes (usuario_id, titulo, descricao, data, endereco_id, imagem, categoria, status) VALUES (3,'Boca de lobo perigosa','Crianças da vizinhança alegam terem visto um palhaço dentro da calçada. Testemunhas notaram estranhos balões nos arredores.','2019-1-17',14,'','Guarda municipal','RESOLVIDA');
 
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario1','2021-3-1','',1);
-INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario2','2021-3-2','',1);
+INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario2','2021-3-2','https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',1);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (4,'Comentario3','2021-3-3','',1);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (2,'Comentario4','2021-3-4','/foto1.jpg',1);
 
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario5','2021-3-5','',2);
-INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (5,'Comentario6','2021-3-6','',2);
+INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (5,'Comentario6','2021-3-6','https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',2);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario7','2021-3-7','',2);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (2,'Comentario8','2021-3-8','/foto2.jpg',2);
 
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (2,'Comentario9','2021-3-9','',3);
-INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (5,'Comentario10','2021-3-10','',3);
+INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (5,'Comentario10','2021-3-10','https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',3);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario11','2021-3-11','',3);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario12','2021-3-12','/foto3.jpg',3);
 
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (2,'Comentario13','2021-3-13','',4);
-INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (4,'Comentario14','2021-3-14','',4);
-INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario15','2021-3-15','',4);
+INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (4,'Comentario14','2021-3-14','https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',4);
+INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (3,'Comentario15','2021-3-15','https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png',4);
 INSERT INTO comentarios (id_usuario_fk, descricao, data, imagem, reclamacao_id) VALUES (5,'Comentario16','2021-3-16','/foto4.jpg',4);
